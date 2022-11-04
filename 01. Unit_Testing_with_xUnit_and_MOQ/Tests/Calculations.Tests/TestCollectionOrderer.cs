aaafe3da99028849a4acd1853c673ce74bd72d1c
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-11-04 13:52:53 +0330

    Creating Assembly Information file for setting orderer class.

---
+++ 01. Unit_Testing_with_xUnit_and_MOQ/Tests/Calculations.Tests/TestCollectionOrderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Calculations.Tests;
+
+public class TestCollectionOrderer : ITestCollectionOrderer
+{
+    public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+    {
+        return testCollections.OrderBy(x => x.DisplayName);
+    }
+}
