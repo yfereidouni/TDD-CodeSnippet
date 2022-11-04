ca5ab40b9ccf0865f09cae7e10138f9475151b74
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-11-04 14:07:41 +0330

    Adding function for checking OddValues

--- 01. Unit_Testing_with_xUnit_and_MOQ/Soures/Calculations/Calculator.cs
+++ 01. Unit_Testing_with_xUnit_and_MOQ/Soures/Calculations/Calculator.cs
@@ -19,5 +19,10 @@ public class Calculator
     }
 
     public List<int> FiboNumbers => new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+
+    public bool IsOdd(int value)
+    {
+        return (value % 2 == 1);
+    }
 }
 
